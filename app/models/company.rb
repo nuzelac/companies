2d969e4de88420094d81b2c250bd540df9cb27f9
Nino Uzelac <ninic0@gmail.com>
2017-11-22 22:27:48 +0100

    Add Company model validators and tests

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,4 @@
 class Company < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :comment, presence: true, length: { minimum: 10, maximum: 10_000 }
 end
--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class CompanyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @company = Company.new(name: 'Test', comment: 'A short comment')
+  end
+
+  test 'valid company' do
+    assert @company.valid?
+  end
+
+  test 'invalid without name' do
+    @company.name = nil
+    refute @company.valid?
+    assert_not_nil @company.errors[:name]
+  end
+
+  test 'invalid without comment' do
+    @company.comment = nil
+    refute @company.valid?
+    assert_not_nil @company.errors[:comment]
+  end
+
+  test 'invalid with too short comment' do
+    @company.comment = 'abc'
+    refute @company.valid?
+    assert_not_nil @company.errors[:comment]
+  end
 end
